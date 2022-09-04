53e806fec921ef9ce35b80721b886af1e99a7652
becaful <beca.a.clarke@gmail.com>
2022-09-04 15:47:13 +0400

    adding dashed line to middle of panel

--- Pong/src/PongPanel.java
+++ Pong/src/PongPanel.java
@@ -8,6 +8,8 @@ import javax.swing.Timer;
 import javax.swing.JPanel;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Stroke;
+import java.awt.BasicStroke;
 
 
 public class PongPanel extends JPanel implements ActionListener, KeyListener {
@@ -42,6 +44,7 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener {
 	@Override
 	public void actionPerformed(ActionEvent event) {
 		update();
+		repaint();
 		
 	}
 	
@@ -52,7 +55,16 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener {
 	@Override
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		g.setColor(Color.WHITE);
-		g.fillRect(20, 20, 100, 100);
+		paintDottedLine(g);
+		
 	}
+	
+	 private void paintDottedLine(Graphics g) {
+	      Graphics2D g2d = (Graphics2D) g.create();
+	         Stroke dashed = new BasicStroke(3, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, 0, new float[]{9}, 0);
+	         g2d.setStroke(dashed);
+	         g2d.setPaint(Color.WHITE);
+	         g2d.drawLine(getWidth() / 2, 0, getWidth() / 2, getHeight());
+	         g2d.dispose();
+	 }
 }
